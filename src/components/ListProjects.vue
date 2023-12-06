9c552ed2d595acfc1d534f0b0d0f6f56f6e53d5c
FraRussotto <russottofrancesco1@gmail.com>
2023-12-06 23:32:33 +0100

    [edit] component Paginator

---
+++ src/components/ListProjects.vue
@@ -0,0 +1,31 @@
+<script>
+
+import { store } from '../data/store';
+
+  export default {
+    name: 'listProjects',
+    data() {
+      return {
+        store
+      }
+    },
+    computed: {},
+    mounted() {},
+    methods: {}
+  }
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="project in store.projects" :key="project.id">
+        <p>{{ project.name }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
---
+++ src/components/partials/Paginator.vue
@@ -0,0 +1,36 @@
+<script>
+
+  export default {
+    name: 'Paginator',
+    props: {
+      links: Array,
+    },
+
+    data() {
+      return {
+      }
+    },
+
+    computed: {},
+
+    mounted() {},
+
+    methods: {
+      getUrl(linkUrl) {
+        this.$emit('getUrl', linkUrl)
+      },
+    }
+  }
+</script>
+
+
+<template>
+    <button>--</button>
+    <button v-for="link in links" :key="link.label" v-html="link.label" :disabled="link.active || !link.url" @click="getUrl(link.url)"></button>
+    <button>--</button>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
