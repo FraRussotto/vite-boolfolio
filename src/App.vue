2881453abf33660e01ab448af85f100144e5139a
FraRussotto <russottofrancesco1@gmail.com>
2023-12-06 18:11:47 +0100

    [edit] Milestone 4: add API from backend

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <script>
 
+import { store } from './data/store';
+import axios from "axios";
+
 export default{
   name: 'App',
   data(){
     return {
       message: 'Hello Vue!'
     }
-  }
+  },
+
+    methods: {
+      getApi() {
+        axios.get(store.apiUrl + 'projects')
+      .then(result => {
+          console.log(result.data);
+        })
+      }
+    },
+
+    mounted() {
+      this.getApi();
+    }
 }
 
 </script>
 
 <template>
 
-  <h1>{{ message }}</h1>
+  
 
 </template>
 
