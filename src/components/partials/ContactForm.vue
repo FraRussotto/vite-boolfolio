fd4eee771642280bd343d9e95b8e1122f00b3db8
FraRussotto <russottofrancesco1@gmail.com>
2023-12-18 15:11:27 +0100

    [fix] errors message

--- src/components/partials/ContactForm.vue
+++ src/components/partials/ContactForm.vue
@@ -9,7 +9,13 @@ import { store } from '../../data/store';
       return {
         name: '',
         email: '',
-        message: ''
+        message: '',
+        success: false,
+        errors: {
+          name:[],
+          email: [],
+          message: []
+        }
       }
     },
     computed: {},
@@ -26,6 +32,10 @@ import { store } from '../../data/store';
         axios.post(store.apiUrl + 'send-mail', data)
           .then(response =>{
             console.log(response.data);
+            this.success = response.data.success;
+            if(!this.success){
+              this.errors = response.data.errors;
+            }
           })
           .catch(error =>{
             console.log(error);
@@ -37,24 +47,30 @@ import { store } from '../../data/store';
 
 <template>
   <div>
-    <form @submit.prevent="sendForm()">
+    <form v-if="!success" @submit.prevent="sendForm()">
 
       <div class="section">
         <label class="my-2" for="name">Name</label>
         <input v-model="name" type="text" name="name" id="name">
+        <span v-for="error in errors.name" :key="error" class="error">{{error}}</span>
       </div>
+
       <div class="section">
         <label class="my-2" for="email">Email</label>
         <input v-model="email" type="text" name="email" id="email">
+        <span v-for="error in errors.email" :key="error" class="error">{{error}}</span>
       </div>
+
       <div class="section">
         <label class="my-2" for="message">Message</label>
         <textarea v-model="message" type="text" name="message" id="message"></textarea>
+        <span v-for="error in errors.message" :key="error" class="error">{{error}}</span>
       </div>
 
       <button type="submit" class="btn btn-warning">Invia</button>
 
     </form>
+    <p v-else>Inviata!</p>
   </div>
 </template>
 
@@ -67,6 +83,9 @@ label{
   margin: 10px auto;
   label{
     display: block;
+  }
+  .error{
+    color: red;
   }
 }
 </style>
